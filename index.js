8cd007de2bf015ac3d467182f0a56ec1b7ffe61b
Kamesh.V <98347138+Kamesh0603@users.noreply.github.com>
2022-09-14 15:20:03 +0530

    Linking JS to HTML

---
+++ index.js
@@ -0,0 +1,17 @@
+function translator(){
+    var inputvalue = document.getElementById("123").value;
+    var op=document.getElementById("456");
+    console.log(inputvalue);
+    
+    var str1="https://api.funtranslations.com/translate/minion.json";
+    var str2="?";
+    var str3="text=";
+    var texttobtanslated = str1+str2+str3+inputvalue;
+    fetch(texttobtanslated)
+    .then((response) => response.json())
+    .then((json)=>{console.log(json.contents.translated);
+    var output = json.contents.translated;
+    op.innerText = output;})
+    .catch(error => console.log(error));
+
+}
